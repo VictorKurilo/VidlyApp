4f24a7b693e8f5171be2d3e27e3eab32fc547623
victor.kurilo <victor.kurilo@gmail.com>
2018-03-02 20:06:28 +0300

    added genres migration data

--- VidlyApp/Models/Movie.cs
+++ VidlyApp/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,27 @@ namespace VidlyApp.Models
 {
     public class Movie
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
     }
 }
