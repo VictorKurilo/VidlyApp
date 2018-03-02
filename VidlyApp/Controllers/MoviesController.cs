f464d94de449868e9e3b4911e4c60ed4fe7fee9d
victor.kurilo <victor.kurilo@gmail.com>
2018-03-02 20:45:03 +0300

    Added Movies From Db

--- VidlyApp/Controllers/MoviesController.cs
+++ VidlyApp/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using VidlyApp.Models;
@@ -9,21 +10,32 @@ namespace VidlyApp.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        public IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Wall-e"}
-            };
+            var movies = _context.Movies.Include(g => g.Genre).SingleOrDefault(m => m.Id == id);
+
+            return View(movies);
         }
+
     }
 }
