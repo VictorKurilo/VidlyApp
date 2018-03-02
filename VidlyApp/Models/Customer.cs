4518201374794c50a8631b71b411decb3d452732
victor.kurilo <victor.kurilo@gmail.com>
2018-03-02 17:54:51 +0300

    added  membershipt types and data to it table

--- VidlyApp/Models/Customer.cs
+++ VidlyApp/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace VidlyApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeID { get; set; }
     }
 }
